1f7a1a53496dcd4baf148557a4d185dd218fe8ce
Pavel Dusatko <pavel.dusatko@ulikeit.cz>
2011-10-06 18:16:49 +0200

    - official RSS feed fixed

--- RevisionsClient/RevisionsClient/Classes/REConstants.m
+++ RevisionsClient/RevisionsClient/Classes/REConstants.m
@@ -8,4 +8,4 @@
 
 #import "REConstants.h"
 
-NSString * const REBaseURL = @"https://old.uhk.cz/rss/?c=fim-zmeny-rozvrh";
+NSString * const REBaseURL = @"http://www.uhk.cz/cs-cz/studium/rozvrhy/zmeny-ve-vyuce-fim/_layouts/listfeed.aspx?List=%7B3835CAB1-F82E-4CA7-AD17-90C7F2D24933%7D&Source=http%3A%2F%2Fwww.uhk.cz%2Fcs-cz%2Fstudium%2Frozvrhy%2Fzmeny-ve-vyuce-fim%2FStranky%2Fdefault.aspx";
